82bb6343b7cc121e4ca091fd911e2691cfe9eb85
howei <hongrenwei@gmail.com>
2015-01-18 23:37:27 -0800

    add Merge Intervals.

--- src/array/MergeIntervals.java
+++ src/array/MergeIntervals.java
@@ -6,13 +6,20 @@ import java.util.List;
 import other.TestMain;
 import other.TestMain.Interval;
 
-public class InsertInterval {
-/**
- * already committed
- * @param intervals
- * @param newInterval
- * @return
- */
+public class MergeIntervals {
+	public static List<Interval> merge(List<Interval> intervals) {
+		if (intervals == null || intervals.size() == 0
+				|| intervals.size() == 1) {
+			return intervals;
+		}
+		
+		List<Interval> res = new ArrayList<Interval>();
+		res.add(intervals.get(0));
+		for (Interval it : intervals) {
+			res = insert(res, it);
+		}
+		return res;
+	}
     public static List<Interval> insert(List<Interval> intervals, Interval newInterval) {
     	List<Interval> res = new ArrayList<Interval>();
     	if (intervals == null || intervals.size() == 0) {
