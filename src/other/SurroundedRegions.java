c75dd0e2a29040ba60f87383909cb1c78546595f
howei <hongrenwei@gmail.com>
2015-02-04 22:33:48 -0800

    change Surrounded Regions.

--- src/other/SurroundedRegions.java
+++ src/other/SurroundedRegions.java
@@ -2,10 +2,7 @@ package other;
 
 import java.util.LinkedList;
 import java.util.Queue;
-/**
- * Need optimization. Time limit exceed 
- *
- */
+
 public class SurroundedRegions {
 	public static void solve(char[][] board) {
 		if (board == null || board.length <= 2 || board[0].length <= 2) {
@@ -19,69 +16,63 @@ public class SurroundedRegions {
 			if (board[0][i] == 'O') {
 				queue.add(0);
 				queue.add(i);
-				setPoundBFS(queue, rows, cols, board);
+				bfs(queue, board, rows, cols);
 			}
 		}
 		for (int i = 1; i < rows; i++) {
 			if (board[i][0] == 'O') {
 				queue.add(i);
 				queue.add(0);
-				setPoundBFS(queue, rows, cols, board);
+				bfs(queue, board, rows, cols);
 			}
 		}
 		for (int i = 1; i < rows; i++) {
 			if (board[i][cols - 1] == 'O') {
 				queue.add(i);
 				queue.add(cols - 1);
-				setPoundBFS(queue, rows, cols, board);
+				bfs(queue, board, rows, cols);
 			}
 		}
 		for (int i = 1; i < cols - 1; i++) {
 			if (board[rows - 1][i] == 'O') {
 				queue.add(rows - 1);
 				queue.add(i);
-				setPoundBFS(queue, rows, cols, board);
+				bfs(queue, board, rows, cols);
 			}
 		}
+
 		printBoard(board);
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
-				if (board[i][j] == '#') {
-					board[i][j] = 'O';
-				} else if (board[i][j] == 'O') {
+				if (board[i][j] == 'O') {
 					board[i][j] = 'X';
+				} else if (board[i][j] == '#') {
+					board[i][j] = 'O';
 				}
 			}
 		}
 		printBoard(board);
 	}
 
-	public static void setPoundBFS(Queue<Integer> queue, int rows,
-			int cols, char[][] board) {
+	public static void bfs(Queue<Integer> queue, char[][] board, int rows, int cols) {
 		while (!queue.isEmpty()) {
 			int row = queue.poll();
 			int col = queue.poll();
+			if (row < 0 || col < 0 || row >= rows || col >= cols || board[row][col] != 'O') {
+				continue;
+			}
 			board[row][col] = '#';
-
-			if (row - 1 >= 0 && board[row - 1][col] == 'O') {
-				queue.add(row - 1);
-				queue.add(col);
-			}
-			if (row + 1 < rows && board[row + 1][col] == 'O') {
-				queue.add(row + 1);
-				queue.add(col);
-			}
-			if (col + 1 < cols && board[row][col + 1] == 'O') {
-				queue.add(row);
-				queue.add(col + 1);
-			}
-			if (col - 1 >= 0 && board[row][col - 1] == 'O') {
-				queue.add(row);
-				queue.add(col - 1);
-			}
+			queue.add(row - 1);
+			queue.add(col);
+			queue.add(row + 1);
+			queue.add(col);
+			queue.add(row);
+			queue.add(col - 1);
+			queue.add(row);
+			queue.add(col + 1);
 		}
 	}
-
+	
 	public static void setPoundDFS(int row, int col, int rows, int cols,
 			char[][] board) {
 		board[row][col] = '#';
